f918e3a9e63948702ac133efde6186627a033357
syedibrahim1016 <106728019+syedibrahim1016@users.noreply.github.com>
2022-09-08 15:55:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<html>
+    <head>
+        <body>
+            <?php
+/* $price = 10;
+echo"syed ibrahim:$price<br>";
+echo"smart guy:$price<br>";
+echo"innovative ideas:$price";
+*/
+
+/*static $fan = 1;//global variable   
+function fun()
+{
+    global $fan;
+$val = 10;
+$fan++;
+$val++;
+echo "$val<br/>";   //local variable
+echo "$fan<br>";
+}
+fun();
+fun();*/   
+
+// this for constant topic
+// in this false used to case sensitive
+// it gives true it will accept the value in anytype case sensitive or in case sensitive
+/*define("VALUE","hi dear sin",false);
+function fun()
+{
+
+
+    echo VALUE;
+}
+
+fun(); */
+
+/*$customer = "female";
+if($customer == "male")
+{
+    echo("you are go right side");
+}
+else if($customer == "female")
+{
+    echo("you are go left side");
+}
+else{
+    echo("you are go to the lift");
+} */
+ 
+// switch case
+
+/* $tablenum = 1;
+switch($tablenum)
+{
+    case 1:
+        echo("table num:1<br/>");
+    break;
+    case 2:
+        echo("table num:2<br/>");
+    break;
+    case 3:
+        echo("table num:3<br/>");
+    break;
+    case 4:
+        echo("table num:4<br/>");
+    break;
+    default:
+        echo "default table";
+} */
+
+//looping concept
+
+/* for($i = 1;$i<=3; $i++)
+{
+    echo "customer : $i<br>";
+    echo "itly :3<br>";
+    echo "dosai :3<br>";
+    
+} */
+
+// for dont know the num , using this format
+/* $i=true;
+while($i != false)
+{
+    echo "customer : $i<br>";
+    echo "itly :3<br>";
+    echo "dosai :3<br>";
+    
+} */
+
+
+
+?>
+        </body>
+      <html>  
